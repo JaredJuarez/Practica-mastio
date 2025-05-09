46d9061d82152221bba7b4eee1f1e4d9b4fd111b
JaredJuarez <jaredjuarez46@gmail.com>
2025-05-09 17:32:27 -0600

    refactor: Remove unused Navbar and Project2 components; update Project1 layout and styles.

--- src/pages/project1/Project1.jsx
+++ src/pages/project1/Project1.jsx
@@ -1,20 +1,26 @@
 import React, { use, useState } from "react";
-import reportData from "../../data/reportData.json"; // Importamos nuestros datos JSON
-import reportData2Years from "../../data/reportData-2years.json"; // Importamos nuestros datos JSON
-import "../project1/Project.css"; // Importaremos el CSS que crearemos luego
+import reportData from "../../data/reportData.json";
+import reportData2Years from "../../data/reportData-2years.json";
+import "../project1/Project.css";
 
 const Project1 = () => {
-  const [showTwoYears, setShowTwoYears] = useState(false); // Estado para mostrar u ocultar el segundo año
+  const [showTwoYears, setShowTwoYears] = useState(false);
   const currentData = showTwoYears ? reportData2Years : reportData;
-  const { report } = currentData; // Desestructuramos el objeto report del JSON importado
-  const { studyAnalysis, name: reportName } = report; // Desestructuramos el objeto report del JSON importado
-  const companies = report.categories[0]?.companies || []; // Asumimos que los datos de las empresas están en la primera categoría
+  const { report } = currentData;
+  const { studyAnalysis, name: reportName } = report;
+  const companies = report.categories[0]?.companies || [];
 
-  // Calcular el valor máximo de score
+  // Calcular el valor máximo de score considerando ambos años
   const maxScore = Math.max(
-    ...companies.map((company) => company.scores[0]?.score || 0)
+    ...companies.map((company) => {
+      const scores = [company.scores[0]?.score || 0];
+      if (showTwoYears) {
+        scores.push(company.scores[1]?.score || 0);
+      }
+      return Math.max(...scores);
+    })
   );
-  // Crear una funcion para colorear la columna de incremento de la tabla, si es positivo, azul, si es negativo, a rojo
+
   const getIncrementColor = (increment) => {
     if (increment > 0) {
       return "#6d84f6";
@@ -50,156 +56,181 @@ const Project1 = () => {
         </div>
       </div>
 
-      <table>
-        <colgroup>
-          <col style={{ width: "5%" }} />
-          <col style={{ width: "20%" }} />
-          {showTwoYears ? (
-            <>
+      <div className="table-container">
+        <div className="table-header">
+          <table>
+            <colgroup>
+              <col style={{ width: "5%" }} />
               <col style={{ width: "10%" }} />
-              <col style={{ width: "10%" }} />
-            </>
-          ) : (
-            <col style={{ width: "20%" }} />
-          )}
-          <col style={{ width: "25%" }} />
-          {showTwoYears ? (
-            <>
-              <col style={{ width: "10%" }} />
-              <col style={{ width: "10%" }} />
-            </>
-          ) : (
-            <col style={{ width: "20%" }} />
-          )}
-          {showTwoYears ? (
-            <>
-              <col style={{ width: "10%" }} />
-              <col style={{ width: "10%" }} />
-            </>
-          ) : (
-            <col style={{ width: "20%" }} />
-          )}
-        </colgroup>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Company</th>
-            {showTwoYears ? (
-              <>
-                <th>Observations 2024</th>
-                <th>Observations 2023</th>
-              </>
-            ) : (
-              <th>Number of observations</th>
-            )}
-            <th>Score Chart</th>
-            {showTwoYears ? (
-              <>
-                <th>Score 2024</th>
-                <th>Score 2023</th>
-              </>
-            ) : (
-              <th>Score</th>
-            )}
-            {showTwoYears ? (
-              <>
-                <th>% Above/Below 2024</th>
-                <th>% Above/Below 2023</th>
-              </>
-            ) : (
-              <th>% Above/Below</th>
-            )}
-          </tr>
-        </thead>
-        <tbody>
-          {companies.map((company, index) => (
-            <tr key={company.id}>
-              <td>{index + 1}</td>
-              <td>{company.name}</td>
               {showTwoYears ? (
                 <>
-                  <td>{company.scores[0]?.responses || "N/A"}</td>
-                  <td>{company.scores[1]?.responses || "N/A"}</td>
+                  <col style={{ width: "10%" }} />
+                  <col style={{ width: "10%" }} />
                 </>
               ) : (
-                <td>{company.scores[0]?.responses || "N/A"}</td>
+                <col style={{ width: "20%" }} />
               )}
-              <td style={{ padding: "0" }}>
-                <div className="progress-bar-container">
-                  <div
-                    className="progress-bar"
-                    style={{
-                      width: `${(company.scores[0]?.score / maxScore) * 100}%`,
-                    }}
-                  ></div>
-                  {showTwoYears && (
-                    <div
-                      className="progress-bar secondary"
-                      style={{
-                        width: `${
-                          (company.scores[1]?.score / maxScore) * 100
-                        }%`,
-                      }}
-                    ></div>
+              <col style={{ width: "25%" }} />
+              {showTwoYears ? (
+                <>
+                  <col style={{ width: "10%" }} />
+                  <col style={{ width: "10%" }} />
+                </>
+              ) : (
+                <col style={{ width: "20%" }} />
+              )}
+              {showTwoYears ? (
+                <>
+                  <col style={{ width: "10%" }} />
+                  <col style={{ width: "10%" }} />
+                </>
+              ) : (
+                <col style={{ width: "20%" }} />
+              )}
+            </colgroup>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Company</th>
+                {showTwoYears ? (
+                  <>
+                    <th>Observations 2024</th>
+                    <th>Observations 2023</th>
+                  </>
+                ) : (
+                  <th>Number of observations</th>
+                )}
+                <th>Score Chart</th>
+                {showTwoYears ? (
+                  <>
+                    <th>Score 2024</th>
+                    <th>Score 2023</th>
+                  </>
+                ) : (
+                  <th>Score</th>
+                )}
+                {showTwoYears ? (
+                  <>
+                    <th>% Above/Below 2024</th>
+                    <th>% Above/Below 2023</th>
+                  </>
+                ) : (
+                  <th>% Above/Below</th>
+                )}
+              </tr>
+            </thead>
+          </table>
+        </div>
+        <div className="table-body-container">
+          <table>
+            <colgroup>
+              <col style={{ width: "5%" }} />
+              <col style={{ width: "10%" }} />
+              {showTwoYears ? (
+                <>
+                  <col style={{ width: "10%" }} />
+                  <col style={{ width: "10%" }} />
+                </>
+              ) : (
+                <col style={{ width: "20%" }} />
+              )}
+              <col style={{ width: "25%" }} />
+              {showTwoYears ? (
+                <>
+                  <col style={{ width: "10%" }} />
+                  <col style={{ width: "10%" }} />
+                </>
+              ) : (
+                <col style={{ width: "20%" }} />
+              )}
+              {showTwoYears ? (
+                <>
+                  <col style={{ width: "10%" }} />
+                  <col style={{ width: "10%" }} />
+                </>
+              ) : (
+                <col style={{ width: "20%" }} />
+              )}
+            </colgroup>
+            <tbody>
+              {companies.map((company, index) => (
+                <tr key={company.id}>
+                  <td>{index + 1}</td>
+                  <td>{company.name}</td>
+                  {showTwoYears ? (
+                    <>
+                      <td>{company.scores[0]?.responses || "N/A"}</td>
+                      <td>{company.scores[1]?.responses || "N/A"}</td>
+                    </>
+                  ) : (
+                    <td>{company.scores[0]?.responses || "N/A"}</td>
                   )}
-                </div>
-              </td>
-              {showTwoYears ? (
-                <>
-                  <td>{company.scores[0]?.score?.toFixed(6) || "N/A"}</td>
-                  <td>{company.scores[1]?.score?.toFixed(6) || "N/A"}</td>
-                </>
-              ) : (
-                <td>{company.scores[0]?.score?.toFixed(6) || "N/A"}</td>
-              )}
-              {showTwoYears ? (
-                <>
-                  <td
-                    style={{
-                      backgroundColor: getIncrementColor(
-                        company.scores[0]?.increment
-                      ),
-                    }}
-                  >
-                    {company.scores[0]?.increment?.toFixed(4) || "N/A"}
+                  <td style={{ padding: "0" }}>
+                    <div className="progress-bar-container">
+                      <div
+                        className="progress-bar"
+                        style={{
+                          width: `${
+                            (company.scores[0]?.score / maxScore) * 100
+                          }%`,
+                        }}
+                      ></div>
+                      {showTwoYears && (
+                        <div
+                          className="progress-bar secondary"
+                          style={{
+                            width: `${
+                              (company.scores[1]?.score / maxScore) * 100
+                            }%`,
+                          }}
+                        ></div>
+                      )}
+                    </div>
                   </td>
-                  <td
-                    style={{
-                      backgroundColor: getIncrementColor(
-                        company.scores[1]?.increment
-                      ),
-                    }}
-                  >
-                    {company.scores[1]?.increment?.toFixed(4) || "N/A"}
-                  </td>
-                </>
-              ) : (
-                <td
-                  style={{
-                    backgroundColor: getIncrementColor(
-                      company.scores[0]?.increment
-                    ),
-                  }}
-                >
-                  {company.scores[0]?.increment?.toFixed(4) || "N/A"}
-                </td>
-              )}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-
-      <div className="score-rule">
-        <div className="score-line">
-          {Array.from(
-            { length: Math.ceil(maxScore) }, // Genera un rango de 0 a maxScore
-            (_, i) => (
-              <span key={i} className="score-tick">
-                {i === Math.ceil(maxScore) ? maxScore.toFixed(2) : i}{" "}
-                {/* Formatea el último número */}
-              </span>
-            )
-          )}
+                  {showTwoYears ? (
+                    <>
+                      <td>{company.scores[0]?.score?.toFixed(6) || "N/A"}</td>
+                      <td>{company.scores[1]?.score?.toFixed(6) || "N/A"}</td>
+                    </>
+                  ) : (
+                    <td>{company.scores[0]?.score?.toFixed(6) || "N/A"}</td>
+                  )}
+                  {showTwoYears ? (
+                    <>
+                      <td
+                        style={{
+                          backgroundColor: getIncrementColor(
+                            company.scores[0]?.increment
+                          ),
+                        }}
+                      >
+                        {company.scores[0]?.increment?.toFixed(4) || "N/A"}
+                      </td>
+                      <td
+                        style={{
+                          backgroundColor: getIncrementColor(
+                            company.scores[1]?.increment
+                          ),
+                        }}
+                      >
+                        {company.scores[1]?.increment?.toFixed(4) || "N/A"}
+                      </td>
+                    </>
+                  ) : (
+                    <td
+                      style={{
+                        backgroundColor: getIncrementColor(
+                          company.scores[0]?.increment
+                        ),
+                      }}
+                    >
+                      {company.scores[0]?.increment?.toFixed(4) || "N/A"}
+                    </td>
+                  )}
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
