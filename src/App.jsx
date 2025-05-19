b743d3162eabaf43acfc374d3f7d08932f234c3b
JaredJuarez <jaredjuarez46@gmail.com>
2025-05-19 14:11:17 -0600

    - Create new proyect (Project2) to implement the json reportData-focusCompany.json. - Implement the narvbar component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Project1 from "./pages/project1/Project1";
+import Project2 from "./pages/project2/Project2"
+import Navbar from "./components/Navbar";
+
 
 const App = () => {
   return (
     <Router>
+      <Navbar />
       <div className="content">
         <Routes>
           <Route path="/" element={<Project1 />} />
+          <Route path="/project2" element={<Project2 />} />
         </Routes>
       </div>
     </Router>
